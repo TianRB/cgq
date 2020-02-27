e164640fcf7d80beee9edc3d0ce44f9fc7fe6e64
José Peña <Pepe@MacBook-Air-de-Jose.local>
2020-02-27 13:51:04 -0600

    asas

--- resources/views/nota.blade.php
+++ resources/views/nota.blade.php
@@ -12,7 +12,7 @@
           <figure><img src="{{ asset($post->image) }}" alt="{{ $post->slug }}"></figure>
       @endif
     <section>
-      <h1>{{ $post->title }}</h1>
+      <h1>{{ $p->title }}</h1>
       {!! $post->content !!}
     </section>
     <div class="fb-comments" data-href="https://www.facebook.com/centro.endoscoquirurgico.ceq/" data-width="100%" data-numposts="5"></div>
